0651dec45f680a735e4f7f332005769c220c8799
mmvES <miguerepvfr@gmail.com>
2025-12-02 09:25:34 +0100

    sngrado game.js

--- Backend/models/Game.js
+++ Backend/models/Game.js
@@ -2,9 +2,11 @@
 const mongoose = require('../database').mongoose;
 
 const gameSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  description: String,
-  createdAt: { type: Date, default: Date.now }
+  name: { type: String,
+          required: true },
+          description: String,
+          createdAt: { type: Date,
+                       default: Date.now }
 });
 
 module.exports = mongoose.model('Game', gameSchema);
